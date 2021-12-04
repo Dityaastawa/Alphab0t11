384892026800c87b608f0529ca56b5111f4145b2
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2021-12-04 08:42:28 +0800

    Add files via upload

---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 085 792 429 140
+2. BCA 
+	ㅁ 085 792 429 140
+3. BNI 
+	ㅁ 085 792 429 140
+4. PERMATA
+	ㅁ 085 792 429 140
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 085 792 429 140
+2. OVO
+	ㅁ 085 792 429 140
+3. DANA
+	ㅁ 085 792 429 140
+	
+Semuanya atas nama DI****AS
+`
+	}
+
+exports.donasibot = donasibot
+ 
