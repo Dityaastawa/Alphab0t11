5f32d502e56c6a0a4232d716e683ba2aa0d4fb32
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2021-12-04 08:41:48 +0800

    Add files via upload

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 085 792 429 140
+2. BCA 
+	ㅁ 085 792 429 140
+3. BNI 
+	ㅁ 085 792 429 140
+4. PERMATA
+	ㅁ 085 792 429 140
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 085 792 429 140
+2. OVO
+	ㅁ 085 792 429 140
+3. DANA
+	ㅁ 085 792 429 140
+	
+Semuanya atas nama RI****DI
+
+*📲 PULSA*
+
+    ㅁ 085 792 429 140
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
